7101dff34b16948989595b00906e39b85c7ac077
Your Name <you@example.com>
2024-08-03 22:12:05 -0400

    Had to discard old test as it only checked for the distinct amongst adjacent characters.

--- C-1dot15.py
+++ C-1dot15.py
@@ -1,19 +1,19 @@
-def distinction_test(seq):
-    if len(seq) < 2:
-        return "This test is unnecessary."
-    else:
-        i = 1
-        l = len(seq)
-        for j in range(i, l):
-            if seq[i - 1] == seq[j]:
-                return False
-            else:
-                i += 1
-        return True
+#def distinction_test(seq):
+#    if len(seq) < 2:
+#        return "This test is unnecessary."
+#    else:
+#        i = 1
+#        l = len(seq)
+#        for j in range(i, l):
+#            if seq[i - 1] == seq[j]:
+#                return False
+#            else:
+#                i += 1
+#        return True
 
-#def are_distinct(sequence):
-#    unique_elements = set(sequence)
-#    return len(unique_elements) == len(sequence)
+def are_distinct(sequence):
+    unique_elements = set(sequence)
+    return len(unique_elements) == len(sequence)
 
 data = [1,2,3,4,5,6]; print (distinction_test(data))
 data = [1,2,3,3,5,6]; print (distinction_test(data))
