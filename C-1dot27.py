49b5105447e2d8f6a90a4f996c2bff23b242b3a9
Your Name <you@example.com>
2024-08-03 22:31:59 -0400

    Just started on this one. Also addition of corrected and optimized code courtesy of AI.

--- C-1dot27.py
+++ C-1dot27.py
@@ -12,3 +12,29 @@ def factors(n):
 #        yield k
 
 factors(100)
+
+#def factors(n):
+#    def smaller_factors():
+#        k = 1
+#        while k * k <= n:
+#            if n % k == 0:
+#                yield k
+#                if k != n // k:
+#                    yield n // k
+#            k += 1
+#    
+#    def larger_factors(small_factors_gen):
+#        # Track factors in increasing order from the first generator
+#        for factor in small_factors_gen:
+#            yield factor
+#        k = int(n ** 0.5) + 1
+#        while k <= n:
+#            if n % k == 0:
+#                if k != n // k:  # Avoid yielding the square root twice if it's a perfect square
+#                    yield n // k
+#            k += 1
+#
+#    # Generate smaller factors and then generate the corresponding larger factors
+#    small_factors_gen = smaller_factors()
+#    yield from larger_factors(small_factors_gen)
+
